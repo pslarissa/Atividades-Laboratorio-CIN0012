9f51031313ca2b334138837f7eac7846bd0cf7eb
Larissa Paiva <lp.larissapaiva@gmail.com>
2026-08-28 22:44:48 -0300

    Update atividade1.asm

--- lab2/atividade1.asm
+++ lab2/atividade1.asm
@@ -1,18 +1,12 @@
 lw x10, a  	#x10 = a
 lw x11, b	#x11 = b 
 lw x12, m	#x12 = m
-
 add x12, x10, x0 # m = a (a + 0)
-
 bge x11, x12, end #compara de b >=m, se for verdadeiro, ele pula para a 'marcacao' "END"
-
-add x12, x10, x11 # m = a + b (sÛ funciona se a condiÁ„o anterior n„o for verdadeira)
-
+add x12, x10, x11 # m = a + b (s√≥ funciona se a condi√ß√£o anterior n√£o for verdadeira)
 end:
 sw x12, m #salva m e encerra
 halt
-
-# Vari·veis
 a: .word 0xE
 b: .word 0x7
 m: .word 0x0
