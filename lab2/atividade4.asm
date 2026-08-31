5f3c101d75df2b0081825b04c6d74be312ecccf1
Júlia Almeida <juliaabdantas@gmail.com>
2026-08-31 19:00:29 -0300

    Update atividade4.asm

--- lab2/atividade4.asm
+++ lab2/atividade4.asm
@@ -1,1 +1,13 @@
+addi x11, x0, 28 #guarda o endereço inicial da string
+   
+loop:
+    lb   x10, 0(x11)
+    beq  x10, x0, end   #se o caractere for 0, sai do loop
+    sb   x10, 1024(x0) 
+    addi x11, x11, 1    #passa pro proximo caractere na memória
+    beq  x0, x0, loop    
 
+end:
+    halt
+
+str1: .string "Hello World"
